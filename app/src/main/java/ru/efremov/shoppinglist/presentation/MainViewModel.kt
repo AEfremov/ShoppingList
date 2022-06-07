a383f837240e73c3779d96363e931ee25f1c3850
ae <alexandr.efremoff@gmail.com>
2022-06-08 02:35:55 +0300

    Presentation layer refactoring.

--- app/src/main/java/ru/efremov/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/ru/efremov/shoppinglist/presentation/MainViewModel.kt
@@ -1,6 +1,5 @@
 package ru.efremov.shoppinglist.presentation
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.efremov.shoppinglist.data.ShopListRepositoryImpl
@@ -17,21 +16,14 @@ class MainViewModel : ViewModel() {
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
-    val shopList = MutableLiveData<List<ShopItem>>()
-
-    fun getShopList() {
-        val list = getShopListUseCase.getShopList()
-        shopList.value = list
-    }
+    val shopList = getShopListUseCase.getShopList()
 
     fun deleteShopItem(shopItem: ShopItem) {
         deleteShopItemUseCase.deleteShopItem(shopItem)
-        getShopList()
     }
 
     fun changeEnableState(shopItem: ShopItem) {
         val newItem = shopItem.copy(enabled = !shopItem.enabled)
         editShopItemUseCase.editShopItem(newItem)
-        getShopList()
     }
 }
