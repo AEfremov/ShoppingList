0702121c938adf87f1e22fd95e82812bd20781e4
ae <alexandr.efremoff@gmail.com>
2022-06-15 20:17:09 +0300

    ShopItemActivity.kt initial. Added new resources.

---
+++ app/src/main/java/ru/efremov/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,13 @@
+package ru.efremov.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.efremov.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
